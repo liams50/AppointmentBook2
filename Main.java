4a5efeb0e46794175012052398af8544ae1baa7d
liams50 <liams50@nycstudents.net>
2025-10-25 11:47:33 -0400

    Updated all test cases

--- Main.java
+++ Main.java
@@ -2,10 +2,15 @@ public class Main {
     public static void main(String[] args) {
         boolean[][] schedule = new boolean[8][60];
         AppointmentBook a = new AppointmentBook(schedule);
-        for (int i = 10; i < 15; i++){
-            schedule[1][i] = true;
-        }
+        
+        for (int i = 10; i < 15; i++) schedule[1][i] = true;
+        for (int i = 30; i < 45; i++) schedule[1][i] = true;
+        for (int i = 50; i < 60; i++) schedule[1][i] = true;
+    
         a.printPeriod(2);
+        System.out.println(a.findFreeBlock(2, 15));
+        System.out.println(a.findFreeBlock(2, 9));
+        System.out.println(a.findFreeBlock(2, 20));
         
         schedule = new boolean[8][60];
         for (int i = 25; i < 30; i++) schedule[1][i] = true;
