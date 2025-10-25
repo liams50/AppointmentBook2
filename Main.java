0a962a27b8124e9264c17774ed6c567ba5abe125
liams50 <liams50@nycstudents.net>
2025-10-25 12:04:50 -0400

    Test cases finished

--- Main.java
+++ Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         boolean[][] schedule = new boolean[8][60];
-        for (int i = 10; i < 15; i++) schedule[1][i] = true;
+        for (int i = 10; i < 15; i++) schedule[1][i] = true; //updates base schedule
         for (int i = 30; i < 45; i++) schedule[1][i] = true;
         for (int i = 50; i < 60; i++) schedule[1][i] = true;
 
         AppointmentBook a = new AppointmentBook(schedule);
         
         a.printPeriod(2);
-        System.out.println(a.findFreeBlock(2, 15));
+        System.out.println(a.findFreeBlock(2, 15)); //first test cases
         System.out.println(a.findFreeBlock(2, 9));
         System.out.println(a.findFreeBlock(2, 20));
         
         schedule = new boolean[8][60];
-        for (int i = 25; i < 30; i++) schedule[1][i] = true;
+        for (int i = 25; i < 30; i++) schedule[1][i] = true; //updates new schedule
         for (int i = 0; i < 15; i++) schedule[2][i] = true;
         for (int i = 41; i < 60; i++) schedule[2][i] = true;
         for (int i = 5; i < 30; i++) schedule[3][i] = true;
@@ -21,16 +21,23 @@ public class Main {
 
         AppointmentBook b = new AppointmentBook(schedule);
 
-        int period = 2;
+        int period = 2; //prints base schedule
         while (period < 5){
             System.out.println("Period: " + period);
             b.printPeriod(period);
             period++;
         }
 
-        b.printPeriod(4);
-        System.out.println(b.makeAppointment(2, 4, 22));
-        b.printPeriod(4);
+        System.out.println(b.makeAppointment(2, 4, 22)); //second test cases
+        System.out.println(b.makeAppointment(3, 4, 3));
+        System.out.println(b.makeAppointment(2, 4, 30));
 
+        int period = 2; //prints schedule after running test cases
+        while (period < 5){
+            System.out.println("Period: " + period);
+            b.printPeriod(period);
+            period++;
+        }
+        
     }
 }
